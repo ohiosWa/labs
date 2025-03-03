1eb4537a46ee1c1a29f3c0e2be76a5bf024a6f1a
ohiosWa <melnikov.aleksey3003@gmail.com>
2025-03-03 16:20:44 +0300

    ver 1

---
+++ lab3.py
@@ -0,0 +1,25 @@
+'task 3.1'
+from random import randint
+import math
+r = randint(1,9)
+S = math.pi*r**2
+print (S)
+'task 3.2'
+a = int(input())
+b = int(input())
+if a != 0:
+    x = -b / a
+    print(x)
+else:print('no reshenie')
+'task 3.3'
+c = int(input())
+F = (9/5) * c + 32
+print (F)
+'task 3.4'
+l = 5 + 2*3 - 4/2
+k = (3+5) * (2+4)/2
+j = -3 + 6/2*4
+h = 5+4*5**2+7
+print (l, k , j , h)
+
+
